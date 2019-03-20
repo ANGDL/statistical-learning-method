7e2ed96f0e0e234679bcfea89d28b3b4c11cc63a
ANG <ang_z@outlook.com>
2019-03-20 10:56:39 +0800

    fix: 修复特征统计bug

--- 3. 朴素贝叶斯/naive_bayes.py
+++ 3. 朴素贝叶斯/naive_bayes.py
@@ -25,9 +25,10 @@ class MultinomialNB:
         for x_i, y_i in zip(X, y):
             self.__counter(self.y_counter, y_i)
             self.n_simples += 1
+            # j 是x的特征维度
             for j, x_i_j in enumerate(x_i):
                 self.sj[j].add(x_i_j)
-                self.__counter(self.joint_xy_counter, (x_i_j, y_i))
+                self.__counter(self.joint_xy_counter, (j, x_i_j, y_i))
 
         return self
 
@@ -35,31 +36,31 @@ class MultinomialNB:
         '''预测'''
         assert len(X[0]) == len(self.sj), 'X dim error'
 
-        prob_list = []
-        prob = [-1.0] * len(self.sj)
+        pred_list = []
+        prob_values = [-1.0] * len(self.sj)
 
         sj = 0
 
         for i, x_i in enumerate(X, 1):
-            prob = dict(zip(self.y_counter.keys(), prob))
+            prob = dict(zip(self.y_counter.keys(), prob_values))
             for y_k in self.y_counter:
                 for j, x_i_j in enumerate(x_i):
                     if self.lambda_ != 0:
                         sj = len(self.sj[j])
                     try:
-                        p_xij_yk = (self.joint_xy_counter[(x_i_j, y_k)] + self.lambda_) / \
+                        p_xij_yk = (self.joint_xy_counter[(j, x_i_j, y_k)] + self.lambda_) / \
                                    (self.y_counter[y_k] + sj * self.lambda_)
                         prob[y_k] *= p_xij_yk
                     except KeyError:
-                        print('Unknown feature: {} in {} dim of simple {}.'.format(x_i_j, j, i))
-                        return None
+                        # print('Unknown feature: {} in {} dim of simple {}.'.format(x_i_j, j, i))
+                        prob[y_k] *= -2
 
                 p_yk = (self.y_counter[y_k] + self.lambda_) / (self.n_simples + len(self.y_counter) * self.lambda_)
                 prob[y_k] *= -p_yk
 
-            prob_list.append(max(prob, key=prob.get))
+            pred_list.append(max(prob, key=prob.get))
 
-        return prob_list
+        return pred_list
 
 
 if __name__ == '__main__':
@@ -76,6 +77,15 @@ if __name__ == '__main__':
     nb_classifier = MultinomialNB()
     nb_classifier.fit(X, y)
 
-    ret = nb_classifier.predict([[5, 'M']])
+    ret = nb_classifier.predict(X)
 
     print(ret)
+
+    # import numpy as np
+    #
+    # X = np.random.randint(5, size=(60, 100))
+    # y = np.array([1, 2, 3, 4, 5, 6]).repeat(10)
+    # nb2 = MultinomialNB()
+    # nb2.fit(X, y)
+    # ret = nb2.predict(X[1:3])
+    # print(ret)
